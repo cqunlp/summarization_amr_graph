cccb9a9e2ac3aaa5135188dc92a9bc3d5549cbac
lvyufeng <lvyufeng@cqu.edu.cn>
2019-09-04 22:05:11 +0800

    add filelist generator

---
+++ utils/filelist.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+
+def _generate_urls_list(file_path,type):
+    f = open(type+'_urls_list.txt','w',encoding = 'utf-8')
+    for root,dirs,files in os.walk(file_path):
+        for i in files:
+            # print(i)
+            if 'story' in i:
+                f.write(os.path.join(root,i)+'\n')
+                # break
+
+    f.close()
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--path',default = '',type = str ,help = 'The source cnn/dailymail dataset story path')
+    arg_parser.add_argument('--type', default = 'cnn', type = str, help = 'dataset type, use cnn or dm')
+    args = arg_parser.parse_args()
+    _generate_urls_list(args.path,args.type)
+    
